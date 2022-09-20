8f51df8cc20375d67c09abc28ab8f21c4945b4ec
Richard Olson <8571182+richardOlson@users.noreply.github.com>
2022-09-19 18:52:24 -0600

    make the test

---
+++ app/src/androidTest/java/com/example/affirmations/AffirmationsListTests.kt
@@ -0,0 +1,9 @@
+package com.example.affirmations
+
+import org.junit.runner.RunWith
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+
+@RunWith(AndroidJUnit4::class)
+class AffirmationsListTests {
+}
